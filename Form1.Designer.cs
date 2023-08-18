c9d3d02bb8eb798a020ca6833211464bde4f419f
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2023-08-18 14:54:54 -0400

    Tela funcionando, porém função do botao n funfando

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panelMenu = new Panel();
+            BtnConfigurações = new Button();
+            BtnTabelaFuncionarios = new Button();
+            BtnFuncionarios = new Button();
+            BtnTabelaProdutos = new Button();
+            BtnProduto = new Button();
+            panelLogo = new Panel();
+            panelMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(224, 81, 53);
+            panelMenu.Controls.Add(BtnConfigurações);
+            panelMenu.Controls.Add(BtnTabelaFuncionarios);
+            panelMenu.Controls.Add(BtnFuncionarios);
+            panelMenu.Controls.Add(BtnTabelaProdutos);
+            panelMenu.Controls.Add(BtnProduto);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(200, 450);
+            panelMenu.TabIndex = 0;
+            // 
+            // BtnConfigurações
+            // 
+            BtnConfigurações.Dock = DockStyle.Top;
+            BtnConfigurações.FlatAppearance.BorderSize = 0;
+            BtnConfigurações.FlatStyle = FlatStyle.Flat;
+            BtnConfigurações.Image = Properties.Resources.configuracoes;
+            BtnConfigurações.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnConfigurações.Location = new Point(0, 275);
+            BtnConfigurações.Name = "BtnConfigurações";
+            BtnConfigurações.Size = new Size(200, 50);
+            BtnConfigurações.TabIndex = 5;
+            BtnConfigurações.Text = "Configurações";
+            BtnConfigurações.UseVisualStyleBackColor = true;
+            BtnConfigurações.Click += BtnConfigurações_Click;
+            // 
+            // BtnTabelaFuncionarios
+            // 
+            BtnTabelaFuncionarios.Dock = DockStyle.Top;
+            BtnTabelaFuncionarios.FlatAppearance.BorderSize = 0;
+            BtnTabelaFuncionarios.FlatStyle = FlatStyle.Flat;
+            BtnTabelaFuncionarios.Image = Properties.Resources.mesa;
+            BtnTabelaFuncionarios.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnTabelaFuncionarios.Location = new Point(0, 225);
+            BtnTabelaFuncionarios.Name = "BtnTabelaFuncionarios";
+            BtnTabelaFuncionarios.Size = new Size(200, 50);
+            BtnTabelaFuncionarios.TabIndex = 4;
+            BtnTabelaFuncionarios.Text = "Tabela Funcionarios";
+            BtnTabelaFuncionarios.UseVisualStyleBackColor = true;
+            BtnTabelaFuncionarios.Click += BtnTabelaFuncionarios_Click;
+            // 
+            // BtnFuncionarios
+            // 
+            BtnFuncionarios.Dock = DockStyle.Top;
+            BtnFuncionarios.FlatAppearance.BorderSize = 0;
+            BtnFuncionarios.FlatStyle = FlatStyle.Flat;
+            BtnFuncionarios.Image = Properties.Resources.do_utilizador;
+            BtnFuncionarios.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnFuncionarios.Location = new Point(0, 175);
+            BtnFuncionarios.Name = "BtnFuncionarios";
+            BtnFuncionarios.Size = new Size(200, 50);
+            BtnFuncionarios.TabIndex = 3;
+            BtnFuncionarios.Text = "Funcionarios";
+            BtnFuncionarios.UseVisualStyleBackColor = true;
+            BtnFuncionarios.Click += BtnFuncionarios_Click;
+            // 
+            // BtnTabelaProdutos
+            // 
+            BtnTabelaProdutos.Dock = DockStyle.Top;
+            BtnTabelaProdutos.FlatAppearance.BorderSize = 0;
+            BtnTabelaProdutos.FlatStyle = FlatStyle.Flat;
+            BtnTabelaProdutos.Image = Properties.Resources.tabela_de_edicao;
+            BtnTabelaProdutos.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnTabelaProdutos.Location = new Point(0, 125);
+            BtnTabelaProdutos.Name = "BtnTabelaProdutos";
+            BtnTabelaProdutos.Size = new Size(200, 50);
+            BtnTabelaProdutos.TabIndex = 2;
+            BtnTabelaProdutos.Text = "Tabela Produtos";
+            BtnTabelaProdutos.UseVisualStyleBackColor = true;
+            BtnTabelaProdutos.Click += BtnTabelaProdutos_Click;
+            // 
+            // BtnProduto
+            // 
+            BtnProduto.Dock = DockStyle.Top;
+            BtnProduto.FlatAppearance.BorderSize = 0;
+            BtnProduto.FlatStyle = FlatStyle.Flat;
+            BtnProduto.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnProduto.Image = Properties.Resources.carrinho_de_compras__2_;
+            BtnProduto.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnProduto.Location = new Point(0, 75);
+            BtnProduto.Name = "BtnProduto";
+            BtnProduto.Size = new Size(200, 50);
+            BtnProduto.TabIndex = 1;
+            BtnProduto.Text = "Produtos";
+            BtnProduto.UseVisualStyleBackColor = true;
+            BtnProduto.Click += BtnProduto_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackgroundImage = Properties.Resources.loja_virtual;
+            panelLogo.BackgroundImageLayout = ImageLayout.Center;
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Enabled = false;
+            panelLogo.ForeColor = Color.White;
+            panelLogo.ImeMode = ImeMode.Off;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(200, 75);
+            panelLogo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelMenu);
+            Name = "Form1";
+            panelMenu.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Button BtnConfigurações;
+        private Button BtnTabelaFuncionarios;
+        private Button BtnFuncionarios;
+        private Button BtnTabelaProdutos;
+        private Button BtnProduto;
+        private Panel panelLogo;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,95 @@ namespace E_Commerce
 {
     public partial class Form1 : Form
     {
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+
+
         public Form1()
         {
+
             InitializeComponent();
+
+        }
+
+
+
+        //Methods
+
+        //Função que faz com que o botão pressionado permaneça em uma cor pré-determinada
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[tempIndex];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        //Função onde irá executar a função SeletcThemeColor
+        private void ActiveButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton == (Button)btnSender)
+                {
+                    //Chamo a função Disable
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    //Chamo a variavel currentButton para 
+                    currentButton = (Button)btnSender;
+                    //Chamo a variavel color e defino como backColor
+                    currentButton.BackColor = color;
+                    //Defino uma cor para o backColor chamando a função Color
+                    currentButton.ForeColor = Color.White;
+                    //Defino a fonte do botão 
+                    currentButton.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+                }
+            }
+        }
+
+        //Função Desativa o botão
+
+        private void DisableButton()
+        {
+            foreach (Control previuosBtn in panelMenu.Controls)
+            {
+                if (previuosBtn.GetType() == typeof(Button))
+                {
+                    previuosBtn.BackColor = Color.FromArgb(14, 42, 86);
+                    previuosBtn.ForeColor = Color.White;
+                    previuosBtn.Font = new Font("Segoe UI", 12.5F, FontStyle.Regular, GraphicsUnit.Point);
+                }
+            }
+        }
+
+        private void BtnConfigurações_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void BtnProduto_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void BtnTabelaProdutos_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void BtnTabelaFuncionarios_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void BtnFuncionarios_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
         }
     }
 }
